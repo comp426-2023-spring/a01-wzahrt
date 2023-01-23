3e0b67c7658bf7447497b397a2e4a03868cfdbfe
wzahrt <wzahrt@unc.edu>
2023-01-23 13:02:55 -0500

    fixed argv and switched it to minimist

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var minimist = require('minimist')(process.argv.slice(2));
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-var port = argv.port || 3000; 
+var port = minimist.port || 3000; 
 
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
