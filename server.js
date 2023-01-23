6ce59bdcd7501d3bd3e34397e333cff1c5c6c6f6
wzahrt <wzahrt@unc.edu>
2023-01-23 12:58:58 -0500

    Worked on the table in index.html and I think I have the server runnning correctly... maybe...

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 // Require http module
+var http = require('http');
 
 // Require fs module
+var fs = require('fs');
 
 // Require minimist module (make sure you install this one via npm).
+var minimist = require('minimist')(process.argv.slice(2));
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 
+
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
+var port = argv.port || 3000; 
 
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
@@ -16,8 +21,11 @@
 
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
-
-
+fs.readFile('./public/index.html', 'utf8', (error, data) => {
+    if(error){
+        console.error(error);
+        return;
+    }
 
 
 
@@ -27,14 +35,20 @@
 // 1. status code 200, 
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
-
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(data);
+})
 
 
 
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
-
+server.listen(port,() => {
+    console.log(`Server listening on port ${port}`)})
+});
 
 
 
